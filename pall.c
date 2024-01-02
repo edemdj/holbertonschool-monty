4ae4c00526821a91106a99a2dc77d209394ea079
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-02 15:43:06 +0100

    add a pall.c

---
+++ pall.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * pall - removes the top element of the stack
+ * @stack: the stack
+ * @line_number: current line number of the bytecode file
+ *
+ * Return: Nothing
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *head;
+    head = *stack;
+
+	while (head != NULL)
+	{
+		printf("%d\n", head->n);
+		head = head->next;
+		
+	}
+}
