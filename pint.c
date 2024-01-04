30bc76609b7f75092ecdc118fba1d8e91ba26feb
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-04 14:57:48 +0100

    modified a pint.c folder

--- pint.c
+++ pint.c
@@ -8,12 +8,18 @@
 */
 void pint(stack_t **stack, unsigned int line_number)
 {
-    
+    stack_t *head;
+
+    head = *stack;
+
     if (*stack == NULL)
     {
         dprintf(2, "L%d: can't pint, stack empty\n", line_number);
         exit(EXIT_FAILURE);
     }
-        printf("%d\n", (*stack)->n);
+    while(head->prev =! NULL)
+        head = head->prev;
+
+    printf("%d\n", head->n);
 
 }
