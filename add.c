cd41e0d2f3e8e785137339c11c3beadbe0ec9c40
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-01-05 16:26:08 +0100

    modified a add folder

---
+++ add.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * add - adds the top two elements of the stack
+ *  @stack: the stack
+ * @line_number: current line number of the bytecode file
+ * @n:integer
+ * Return: Nothing
+*/
+void _add(stack_t **stack, unsigned int line_number, char *value)
+{
+	/*(void)value;*/
+	int sum = 0;
+
+    if (!*stack || !(*stack)->next)
+	{
+		dprintf(2, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if (!*stack || !(*stack)->next)
+   sum = (*stack)->n + (*stack)->next->n;
+	op_pop(stack, line_number);
+	(*stack)->n = sum;
+
+}
