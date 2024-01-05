e8e68dc5dd680d02b23143e7103fab44933e1df0
Ezzud <contact@ezzud.fr>
2024-01-05 06:24:07 -0800

    Fix segmentation faults on no arg push

--- op_push.c
+++ op_push.c
@@ -1,6 +1,24 @@
 #include "monty.h"
 
 /**
+ * numbersOnly - Check if a string is a number
+ * @arg: String to check
+ * Return: 1 if a number, 0 if not
+*/
+int numbersOnly(char *arg)
+{
+	int i;
+
+	for (i = 0; arg[i]; i++)
+	{
+		if (arg[i] == '-' && i == 0)
+			continue;
+		if (isdigit(arg[i]) == 0)
+			return (1);
+	}
+	return (0);
+}
+/**
  * op_push - Push a node in a stack
  * @stack: Stack data
  * @line_number: Actual line of the monty file
@@ -19,14 +37,14 @@ void op_push(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	arg = strtok(NULL, " ");
-	n = atoi(arg);
-	if (n == 0)
+	arg = strtok(NULL, "\n\t\r ");
+	if (arg == NULL || numbersOnly(arg))
 	{
-		dprintf(2, "L%i: usage: push integer\n", line_number);
+		dprintf(1, "L%u: usage: push integer\n", line_number);
 		free(new);
 		exit(EXIT_FAILURE);
 	}
+	n = atoi(arg);
 
 	new->n = n;
 	new->prev = NULL;
