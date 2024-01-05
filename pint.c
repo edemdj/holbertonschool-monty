22e772435cdc6263f02789f6c641f8c08c3b0921
Ezzud <contact@ezzud.fr>
2024-01-05 05:03:51 -0800

    Fix betty compliance : pint.c

--- pint.c
+++ pint.c
@@ -10,19 +10,21 @@
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-    stack_t *head = NULL;
+	stack_t *head = NULL;
 
-    if (*stack == NULL)
-    {
-        dprintf(2, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL)
+	{
+		dprintf(2, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    head = *stack;
- 
-    head->next = NULL;
-    free(head);
+	head = *stack;
 
-    if (*stack)
-        (*stack)->next = NULL;
+	head->next = NULL;
+	free(head);
+
+	if (*stack)
+	{
+		printf("%d\n", head->n, line_number);
+	}
 }
